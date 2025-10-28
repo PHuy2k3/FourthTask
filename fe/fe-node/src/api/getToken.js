f32b38d063c82e834dd6afd858f465813a900d46
Huy <nguyenphanhuy1803@gmail.com>
2025-10-28 12:08:09 +0700

    h2810

--- fe/fe-node/src/api/getToken.js
+++
@@ -1,7 +0,0 @@
-export function getToken() {
-  const local = localStorage.getItem("user");
-  if (local) {
-    return JSON.parse(local).accessToken;
-  }
-  return "";
-}
--- fe/fe-node/src/constants/storageKeys.js
+++
@@ -1,2 +0,0 @@
-export const VIETQR_ORDER_KEY = "vietqrOrderId";
-export const VIETQR_BOOKING_KEY = "vietqrBookingData";
