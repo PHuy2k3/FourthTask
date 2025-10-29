a10a00f099e9af3941a301c99a5cb8f42d355bc5
Huy <nguyenphanhuy1803@gmail.com>
2025-10-29 15:41:06 +0700

    huy291025

--- be/Cinema/Cinema/obj/Debug/net9.0/Cinema.AssemblyInfo.cs
+++ be/Cinema/Cinema/obj/Debug/net9.0/Cinema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cinema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4cbc8a0d066401a2114901c70906d73f90be534")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2e22d111f608af6c862a8ea57aebfe1ca3241f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cinema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cinema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
